8e3fad4c4bec6819df72d6b1ca99021302f54324
yuladp1 <yuladp1@gmail.com>
2022-12-14 20:46:04 +0100

    final variant html+css

--- src/App.vue
+++ src/App.vue
@@ -362,70 +362,82 @@
     <footer class="footer">
       <div class="footer__top">
         <div class="footer__container _container">
-          <div class="footer__column">
-            <div class="footer__label">Company Info</div>
-            <nav class="footer__menu menu-footer">
-              <ul class="menu-footer__list">
-                <li class="menu-footer__item"><a href="">About Us</a></li>
-                <li class="menu-footer__item"><a href="">Carrier</a></li>
-                <li class="menu-footer__item"><a href="">We are hiring</a></li>
-                <li class="menu-footer__item"><a href="">Blog</a></li>
-              </ul>
-            </nav>
-          </div>
-          <div class="footer__column">
-            <div class="footer__label">Company Info</div>
-            <nav class="footer__menu menu-footer">
-              <ul class="menu-footer__list">
-                <li class="menu-footer__item"><a href="">About Us</a></li>
-                <li class="menu-footer__item"><a href="">Carrier</a></li>
-                <li class="menu-footer__item"><a href="">We are hiring</a></li>
-                <li class="menu-footer__item"><a href="">Blog</a></li>
-              </ul>
-            </nav>
-          </div>
-          <div class="footer__column">
-            <div class="footer__label">Company Info</div>
-            <nav class="footer__menu menu-footer">
-              <ul class="menu-footer__list">
-                <li class="menu-footer__item"><a href="">About Us</a></li>
-                <li class="menu-footer__item"><a href="">Carrier</a></li>
-                <li class="menu-footer__item"><a href="">We are hiring</a></li>
-                <li class="menu-footer__item"><a href="">Blog</a></li>
-              </ul>
-            </nav>
-          </div>
-          <div class="footer__column">
-            <div class="footer__label">Company Info</div>
-            <nav class="footer__menu menu-footer">
-              <ul class="menu-footer__list">
-                <li class="menu-footer__item"><a href="">About Us</a></li>
-                <li class="menu-footer__item"><a href="">Carrier</a></li>
-                <li class="menu-footer__item"><a href="">We are hiring</a></li>
-                <li class="menu-footer__item"><a href="">Blog</a></li>
-              </ul>
-            </nav>
-          </div>
-          <div class="footer__column">
-            <div class="footer__label">Get In Touch</div>
-            <div class="footer__contacts">
-              <a href="tel:4805550103">(480) 555-0103</a>
-              <a href="">4517 Washington Ave. Manchester, Kentucky 39495</a>
-              <a href="mailto:debra.holt@example.com">debra.holt@example.com</a>
+          <div class="footer__main">
+            <div class="footer__column">
+              <div class="footer__label">Company Info</div>
+              <nav class="footer__menu menu-footer">
+                <ul class="menu-footer__list">
+                  <li class="menu-footer__item"><a href="">About Us</a></li>
+                  <li class="menu-footer__item"><a href="">Carrier</a></li>
+                  <li class="menu-footer__item"><a href="">We are hiring</a></li>
+                  <li class="menu-footer__item"><a href="">Blog</a></li>
+                </ul>
+              </nav>
+            </div>
+            <div class="footer__column">
+              <div class="footer__label">Company Info</div>
+              <nav class="footer__menu menu-footer">
+                <ul class="menu-footer__list">
+                  <li class="menu-footer__item"><a href="">About Us</a></li>
+                  <li class="menu-footer__item"><a href="">Carrier</a></li>
+                  <li class="menu-footer__item"><a href="">We are hiring</a></li>
+                  <li class="menu-footer__item"><a href="">Blog</a></li>
+                </ul>
+              </nav>
+            </div>
+            <div class="footer__column">
+              <div class="footer__label">Legal</div>
+              <nav class="footer__menu menu-footer">
+                <ul class="menu-footer__list">
+                  <li class="menu-footer__item"><a href="">About Us</a></li>
+                  <li class="menu-footer__item"><a href="">Carrier</a></li>
+                  <li class="menu-footer__item"><a href="">We are hiring</a></li>
+                  <li class="menu-footer__item"><a href="">Blog</a></li>
+                </ul>
+              </nav>
+            </div>
+            <div class="footer__column">
+              <div class="footer__label">Resources</div>
+              <nav class="footer__menu menu-footer">
+                <ul class="menu-footer__list">
+                  <li class="menu-footer__item"><a href="">About Us</a></li>
+                  <li class="menu-footer__item"><a href="">Carrier</a></li>
+                  <li class="menu-footer__item"><a href="">We are hiring</a></li>
+                  <li class="menu-footer__item"><a href="">Blog</a></li>
+                </ul>
+              </nav>
+            </div>
+            <div class="footer__column">
+              <div class="footer__label">Get In Touch</div>
+              <div class="footer__contacts">
+                <a class="footer__contacts-itemphone" href="tel:4805550103"
+                  >(480) 555-0103</a
+                >
+                <a class="footer__contacts-adress" href=""
+                  >4517 Washington Ave. Manchester, Kentucky 39495</a
+                >
+                <a class="footer__contacts-mail" href="mailto:debra.holt@example.com"
+                  >debra.holt@example.com</a
+                >
+              </div>
             </div>
           </div>
         </div>
       </div>
       <div class="footer-social">
         <div class="footer-social__container _container">
-          <h4 class="footer-social-text">
-            Made With Love By Figmaland All Right Reserved
-          </h4>
-          <div class="footer-social__icons">
-            <a href=""> <img src="./assets/images/footer-bottom/1.svg" alt="" /></a>
-            <a href=""> <img src="./assets/images/footer-bottom/2.svg" alt="" /></a>
-            <a href=""> <img src="./assets/images/footer-bottom/3.svg" alt="" /></a>
-            <a href=""> <img src="./assets/images/footer-bottom/4.svg" alt="" /></a>
+          <div class="footer-social__main-wrapper">
+            <div class="footer-social__main">
+              <h4 class="footer-social-text">
+                Made With Love By Figmaland All Right Reserved
+              </h4>
+              <div class="footer-social__icons">
+                <a href=""> <img src="./assets/images/footer-bottom/1.svg" alt="" /></a>
+                <a href=""> <img src="./assets/images/footer-bottom/2.svg" alt="" /></a>
+                <a href=""> <img src="./assets/images/footer-bottom/3.svg" alt="" /></a>
+                <a href=""> <img src="./assets/images/footer-bottom/4.svg" alt="" /></a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -611,6 +623,13 @@ page {
   background-color: #ff7b47;
   color: white;
 }
+.main-block__button-orange:hover {
+  background-color: white;
+  color: #ff7b47;
+  border: 1px solid #ff7b47;
+  transition: all 0.3s ease 0.2s;
+}
+
 @media (max-width: 992px) {
   .main-block__button-orange {
     margin-bottom: 10px;
@@ -619,6 +638,11 @@ page {
 .main-block__button-border {
   border: 1px solid #0d5c63;
   color: #0d5c63;
+}
+.main-block__button-border:hover {
+  background: #0d5c63;
+  color: white;
+  transition: all 0.3s ease 0.2s;
 }
 ._ibg {
   position: relative;
@@ -775,6 +799,7 @@ page {
   align-items: flex-end;
   justify-content: center;
 }
+
 .advantages__value {
   font-weight: 700;
   font-size: 40px;
@@ -1219,4 +1244,108 @@ page {
   background: rgb(160, 62, 5);
 }
 /* FOOTER */
+
+.footer__top {
+  background-color: #17213c;
+  color: white;
+  padding: 50px 0;
+}
+footer__container {
+  margin: 0 -15px;
+}
+.footer__main {
+  display: flex;
+  padding: 0 15px;
+  flex-wrap: wrap;
+}
+.footer__column {
+  flex: 0 1 16.667%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  min-width: 148px;
+}
+.footer__column:last-child {
+  flex: 0 1 33.334%;
+}
+@media (max-width: 950px) {
+  .footer__column {
+    flex: 0 1 25%;
+  }
+  .footer__column:last-child {
+    flex: 0 1 100%;
+  }
+}
+.footer__label {
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 24px;
+  letter-spacing: 0.1px;
+  padding-bottom: 10px;
+}
+
+.menu-footer__list {
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
+  align-items: flex-start;
+}
+.menu-footer__list > * {
+  padding: 10px 0 0 0;
+}
+.menu-footer__item a {
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 24px;
+  letter-spacing: 0.2px;
+  color: #ffffff;
+}
+.menu-footer__item a:hover {
+  text-decoration: underline;
+}
+.footer__contacts {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+.footer__contacts > a {
+  margin: 12px 0 0 0;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 24px;
+  color: white;
+  padding-left: 34px;
+}
+.footer__contacts-itemphone {
+  background: url("./assets/images/footer-top/1.svg") left no-repeat;
+}
+.footer__contacts-adress {
+  background: url("./assets/images/footer-top/2.svg") left no-repeat;
+  text-align: left;
+}
+.footer__contacts-mail {
+  background: url("./assets/images/footer-top/3.svg") left no-repeat;
+}
+.footer-social__main-wrapper {
+  margin: 0 -15px;
+}
+.footer-social__main {
+  display: flex;
+  justify-content: space-between;
+  padding: 20px 30px;
+  align-items: center;
+}
+.footer-social-text {
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 24px;
+  color: #737373;
+  padding-right: 15px;
+}
+.footer-social__icons {
+  display: flex;
+}
+.footer-social__icons > a:not(:last-child) {
+  padding: 0 20px 0 0;
+}
 </style>
